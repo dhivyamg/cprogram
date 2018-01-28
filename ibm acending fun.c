1355736da7cb7583f737c9dd316ccdfcd1d0e25f
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-01-28 13:28:05 +0530

    Create ibm acending fun.c

---
+++ ibm acending fun.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int asending();
+int main()
+{
+	asending();
+}
+int asending()
+{
+	int i,j,temp,a[10];
+	for(i=1;i<=5;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=1;i<=5;i++)
+{
+	for(j=i+1;j<=5;j++)
+	{
+		if(a[i]>a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+	}
+}
+printf("acending no are ");
+for(i=1;i<=5;i++)
+{
+	printf("%d",a[i]);
+}
+	return 0;
+}
+
