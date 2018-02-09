c0ccd8ca1560083c979443b30032ec15007d9446
dhivyamg <31499236+dhivyamg@users.noreply.github.com>
2018-02-09 17:00:09 +0530

    Create middlestring.c

---
+++ middlestring.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+#include<math.h>
+int main(void) {
+	char a[67];
+	float k;
+	int l,h;
+	printf("enter the element");
+	scanf("%s",a);
+	l=strlen(a);
+	k=l/2;
+	if((k%2)==0)
+	{
+		middle=
+	}
+
+	return 0;
+}
